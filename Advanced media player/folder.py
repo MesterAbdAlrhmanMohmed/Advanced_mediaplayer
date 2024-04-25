21b7fb128de8606c612bd53c106b4b9420c722f2
abd alrhman mohamed gad alrab <162161775+MesterAbdAlrhmanMohmed@users.noreply.github.com>
2024-04-25 09:07:04 +0200

    Best media player
    this is a online and offline media player

--- Advanced media player/folder.py
+++ Advanced media player/folder.py
@@ -1,7 +1,7 @@
 from PyQt6 import QtWidgets as qt
 from PyQt6 import QtGui as qt1
 from PyQt6 import QtCore as qt2
-import folder_a,folder_i,folder_v
+import folder_a,folder_i,folder_v,folder_all
 class dialog(qt.QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -11,6 +11,7 @@ class dialog(qt.QDialog):
         self.القائمة.addItem("الصوتيات")
         self.القائمة.addItem("الفيديوهات")
         self.القائمة.addItem("الصور")
+        self.القائمة.addItem("فتح مجلد يحتوي على فيديوهات وصوتيات")
         qt1.QShortcut("return",self).activated.connect(self.s)
         self.تنبيه=qt.QLineEdit()
         self.تنبيه.setReadOnly(True)
@@ -27,3 +28,5 @@ class dialog(qt.QDialog):
             folder_v.dialog(self).exec()
         if العناصر==2:
             folder_i.dialog(self).exec()
+        if العناصر==3:
+            folder_all.dialog(self).exec()
